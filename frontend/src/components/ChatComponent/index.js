8c72f6b0a75e100e69c96378965b12fe92ede86b
Rafael <rafael_guiimaraes@hotmail.com>
2020-05-02 14:13:43 -0300

    deixando padrão as datas que vem do banco

--- frontend/src/components/ChatComponent/index.js
+++ frontend/src/components/ChatComponent/index.js
@@ -58,14 +58,15 @@ export default function ChatComponent({
     const day = now.getDate() <= 9 ? `0${now.getDate()}` : now.getDate();
     const month =
       now.getMonth() + 1 <= 9 ? `0${now.getMonth() + 1}` : now.getMonth() + 1;
-    const year = now.getFullYear();
+    const newYear = now.getYear();
+    const year = newYear.toString().substring(1, 3);
 
     const hour = now.getHours() <= 9 ? `0${now.getHours()}` : now.getHours();
     const minutes = now.getMinutes();
 
     if (message.trim()) {
       socket.emit("chat.message", {
-        date: `${year}/${month}/${day}`,
+        date: `${day}/${month}/${year}`,
         username: usernameState,
         hour: `${hour}:${minutes}`,
         content: message,
@@ -81,8 +82,16 @@ export default function ChatComponent({
 
   function formatDate(date) {
     if (date.length > 10) {
-      const newDate = date.split("T");
-      return newDate[0];
+      const dateYYYYMMDD = date.split("T");
+      const newDate = dateYYYYMMDD[0].split("-");
+      const day = newDate[2];
+      const month = newDate[1];
+      const yearYYYY = newDate[0];
+      const year = yearYYYY.split("");
+
+      // console.log("tesete date", dateYYYYMMDD);
+      // console.log("yeaaaaar", year.split(""));
+      return `${day}/${month}/${year[2]}${year[3]}`;
     } else {
       return date;
     }
